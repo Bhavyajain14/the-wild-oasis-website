c8564aa1859e9761e0c769a427d8584660afc396
Bhavyajain14 <bhavyajain999478@gmail.com>
2025-01-20 23:38:28 +0530

    Filter cabins

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,20 @@
 import CabinList from "../_components/CabinList";
+import Filter from "../_components/Filter";
 import Spinner from "../_components/Spinner";
 import { Suspense } from "react";
 
-export const revalidate = 3600;
+// export const revalidate = 3600; - because it is now dynamically rendered
 // export const revalidate = 15;
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  // console.log(searchParams);
+
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +28,11 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
